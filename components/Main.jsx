1629294b9875a19109465823e2157aa38e66db01
mohammedhayaly <mohammed.alhayali88@yahoo.com>
2023-12-27 23:17:57 +0300

    adding main component

---
+++ components/Main.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Link from 'next/link'
+
+const Main = () => {
+  return (
+    <div>
+ <h1>Company Name</h1>
+      <p>Welcome to our website!</p>
+      <Link href="/about">
+       
+      </Link>
+      <Link href="/services">
+        
+      </Link>
+      <Link href="/contact">
+        
+      </Link>
+          </div>
+  )
+}
+
+export default Main
