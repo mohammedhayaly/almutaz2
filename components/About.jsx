534b5a4a07f7fe8f6332ed9d8f459273c3ac2041
mohammedhayaly <mohammed.alhayali88@yahoo.com>
2023-12-27 23:26:46 +0300

    adding about conponent

---
+++ components/About.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+
+
+        <p>image </p>
+        <p>header</p>
+        <p>article</p>
+
+
+      
+    </div>
+  )
+}
+
+export default About
